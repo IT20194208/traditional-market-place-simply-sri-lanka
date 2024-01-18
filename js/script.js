0914de077bfaf44769a416f76dc314785f959a7b
Ranasinghe R.P.C.P <73824269+IT20194208@users.noreply.github.com>
2024-01-18 14:09:18 +0530

    home to index.html

--- js/script.js
+++ js/script.js
@@ -9,4 +9,12 @@ window.onscroll = () =>{
 
     menu.classList.remove('fa-times');
     navbar.classList.toggle('active');
+
+    if(window.scrollY > 60){
+        document.querySelector('#scroll-top').classList.add('active');
+    }
+    else{
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
 }
+
